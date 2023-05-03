279391ec18d23948fdd40715a6e9f06cc61ab2f6
will1490 <willseghers@gmail.com>
2023-05-03 15:20:35 +0200

    correction display in web page

--- script.js
+++ script.js
@@ -7,22 +7,21 @@ And display the date and time of these cities along with the time and date of Br
 */
 
 let date = new Date();
-let brusselsTime = date.toLocaleString('en-US', { timeZone: 'Europe/Brussels' });
+let brusselsTime = date.toLocaleString('fr-BE', { timeZone: 'Europe/Brussels' });
 
 let anchorageTimezone = 'America/Anchorage';
 let reykjavikTimezone = 'Atlantic/Reykjavik';
 let stPetersburgTimezone = 'Europe/Moscow';
 
 // Create new date objects
-let anchorageDate = new Date().toLocaleString('en-US', { timeZone: anchorageTimezone });
-let reykjavikDate = new Date().toLocaleString('en-US', { timeZone: reykjavikTimezone });
-let stPetersburgDate = new Date().toLocaleString('en-US', { timeZone: stPetersburgTimezone });
+let anchorageDate = new Date().toLocaleString('fr-BE', { timeZone: anchorageTimezone });
+let reykjavikDate = new Date().toLocaleString('fr-BE', { timeZone: reykjavikTimezone });
+let stPetersburgDate = new Date().toLocaleString('fr-BE', { timeZone: stPetersburgTimezone });
 
-// Display the date and time of cities with Brussels date and time
-console.log('Date and time in Anchorage, USA: ' + anchorageDate);
-console.log('Date and time in Reykjavik, Iceland: ' + reykjavikDate);
-console.log('Date and time in Saint-Petersburg, Russia: ' + stPetersburgDate);
-console.log('Date and time in Brussels, Belgium: ' + brusselsTime);
+// Display the date and time of the cities with Brussels date and time
+let affichage = document.querySelector('.affichage');
+
+
 
 /* Exercise 2
 A => Using timestamps, find how many days have passed since the date of your birth. Feeling old, yet?
@@ -30,21 +29,27 @@ B => Write a function to find how many days have passed since any point in time 
 */
 
 // A =>
-let birthdate = '1969-08-28';
-
-let daysSinceBirth = Math.floor((new Date() - new Date(birthdate)) / (1000 * 60 * 60 * 24));
-
-console.log('Number of days since my birth: ' + daysSinceBirth);
+let birthDate = '1969-08-28';
+let daysSinceBirth = Math.floor((new Date() - new Date(birthDate)) / (1000 * 60 * 60 * 24));
 
 // B =>
 function daysSince(date) {
-    // Convert the date string to a timestamp in milliseconds
-    const timestamp = new Date(date).getTime();
-    
-    // Calculate the number of days passed since the timestamp
-    let days = Math.floor((new Date() - timestamp) / (1000 * 60 * 60 * 24));
-    
-    return days;
-  }
-  console.log('Days since 1945-06-06: ' + daysSince('1945-06-06'));
-  
+	// Convert the date string to a timestamp in milliseconds
+	const timestamp = new Date(date).getTime();
+		    
+	// Calculate the number of days passed since the timestamp
+	let days = Math.floor((new Date() - timestamp) / (1000 * 60 * 60 * 24));
+		    
+	return days;
+}
+
+// Display "affichage"
+
+affichage.innerHTML = 'Exercise 1:<br>' +
+    'Date and time in Anchorage, USA: ' + anchorageDate + '<br>' +
+    'Date and time in Reykjavik, Iceland: ' + reykjavikDate + '<br>' +
+    'Date and time in Saint-Petersburg, Russia: ' + stPetersburgDate + '<br>' +
+    'Date and time in Brussels, Belgium: ' + brusselsTime + '<br><br>' +
+    'Exercise 2:<br>' +
+    'Number of days since my birth: ' + daysSinceBirth + '<br>' +
+    'Days since 1975-10-06: ' + daysSince('1975-10-06');
